36c0c3d36314d96ae3e86462f7fc4cf2e627e0fc
Vojtěch Dobeš <vojtech@contentkingapp.com>
2019-12-05 08:45:11 +0100

    Remove constraints on \@param annotation

--- DotBlue/Sniffs/PhpDoc/MethodParametersSniff.php
+++ DotBlue/Sniffs/PhpDoc/MethodParametersSniff.php
@@ -21,45 +21,8 @@ class MethodParametersSniff implements PHP_CodeSniffer\Sniffs\Sniff
 	{
 		$tokens = $phpcsFile->getTokens();
 
-		if ($tokens[$stackPtr]['content'] === '@param') {
-			$this->processParam($phpcsFile, $stackPtr, $tokens);
-		}
-
 		if ($tokens[$stackPtr]['content'] === '@return') {
 			$this->processReturn($phpcsFile, $stackPtr, $tokens);
-		}
-
-	}
-
-
-
-	private function processParam(PHP_CodeSniffer\Files\File $phpcsFile, $stackPtr, $tokens)
-	{
-		$whitespace = $tokens[$stackPtr + 1]['content'];
-
-		if ($whitespace !== '  ') {
-			$fix = $phpcsFile->addFixableError('There must be two spaces between @param and type. Found %s.', $stackPtr, 'Whitespace', [strlen($whitespace)]);
-
-			if ($fix) {
-				$phpcsFile->fixer->beginChangeset();
-				$phpcsFile->fixer->addContent($stackPtr + 1, ' ');
-				$phpcsFile->fixer->endChangeset();
-			}
-		}
-
-		$paramDefinition = $tokens[$stackPtr + 2]['content'];
-
-		$foundProblems = array_filter(explode(' ', $paramDefinition), function ($part) {
-			return strpos($part, '$') === 0;
-		});
-		if ($foundProblems) {
-			$fix = $phpcsFile->addFixableError('Variable names in method\'s DocBlock comments are not allowed.', $stackPtr, 'ParameterName');
-
-			if ($fix) {
-				$phpcsFile->fixer->beginChangeset();
-				$phpcsFile->fixer->replaceToken($stackPtr + 2, explode(' ', $paramDefinition)[0]);
-				$phpcsFile->fixer->endChangeset();
-			}
 		}
 	}
 
